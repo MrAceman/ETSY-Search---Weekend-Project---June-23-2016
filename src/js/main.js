29593d85b142d302e80acaf2e0165c1a5156c600
Niclas Willems <niclas.willems@gmail.com>
2016-06-26 14:04:36 -0400

    Working on footer.  Still having a display issue with imported JSON data due to CSS styling.

--- src/js/main.js
+++ src/js/main.js
@@ -3,19 +3,30 @@ import {
     items
 } from "./items.js";
 
+// function item_template(item) {
+//     return `<div class="items_container">
+//   <div class="item_con"><div class="item_con_img">
+//   <img src="${item.Images[0].url_fullxfull}"></div><div class="item_con_text">
+//   <p><a href=${item.url} class="item_title_url">${item.title}</a></p>
+//   <a href=${item.url} class="item_price_url">${item.price}</a>
+//   <a href=${item.Shop.url} class="shop_url">${item.Shop.shop_name}</a></div>
+//   </div>
+//   </div>`;
+// };
+
 function item_template(item) {
-    return `<div class="items_container">
-  <div class="item_con"><div class="item_con_img">
+    return `<div class="item_con"><div class="item_con_img">
   <img src="${item.Images[0].url_fullxfull}"></div><div class="item_con_text">
-  <p><a href=${item.url} class="item_title_url">${item.title}</a></p>
-  <a href=${item.url} class="item_price_url">${item.price}</a>
+  <a href=${item.url} class="item_title_url">${item.title}</a>
+  <a href=${item.url} class="item_price_url">$${item.price}</a>
   <a href=${item.Shop.url} class="shop_url">${item.Shop.shop_name}</a></div>
-  </div>
   </div>`;
 };
 
+
 var itemHTML = items.results.map(item_template);
 
-var node = $(".item_con");
+// var node = $(".item_con");
+var node = $(".main_container");
 
 node.append(itemHTML);
