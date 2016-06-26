32718724bea2554e45f9a1535fad8af27699b2e6
Niclas Willems <niclas.willems@gmail.com>
2016-06-26 15:56:58 -0400

    Working import and styling of imported data.  Bottom footer done.  Work progressing on related results footer area.

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,7 @@ import {
     items
 } from "./items.js";
 
-// function item_template(item) {
-//     return `<div class="items_container">
-//   <div class="item_con"><div class="item_con_img">
-//   <img src="${item.Images[0].url_fullxfull}"></div><div class="item_con_text">
-//   <p><a href=${item.url} class="item_title_url">${item.title}</a></p>
-//   <a href=${item.url} class="item_price_url">${item.price}</a>
-//   <a href=${item.Shop.url} class="shop_url">${item.Shop.shop_name}</a></div>
-//   </div>
-//   </div>`;
-// };
+// The following code populates the search results
 
 function item_template(item) {
     return `<div class="item_con"><div class="item_con_img">
@@ -23,10 +14,27 @@ function item_template(item) {
   </div>`;
 };
 
-
 var itemHTML = items.results.map(item_template);
 
-// var node = $(".item_con");
-var node = $(".main_container");
+// var node = $(".main_container");works
+  var node = $(".items_container");
+
 
 node.append(itemHTML);
+
+// The previous code populates the main search results
+
+// The following code populates the related to search topics
+
+function related_template(item) {
+    return `<div class="related_to_search"><div class="relateditem">
+  <li class=related_item">${item.tags[0]}</div>
+  </div>`;
+};
+
+
+var relatedHTML = items.results.map(related_template);
+
+var relatednode = $(".related_to_search");
+
+relatednode.append("Related to whatever you searched for" + relatedHTML);
